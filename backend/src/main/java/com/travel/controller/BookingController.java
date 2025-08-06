63c34e747493e71a22cb7041c2a954a35d1b064f
Kaushal Rohankar <kaushalrohankar0@gmail.com>
2025-08-06 07:50:48 +0530

    complete mybooking

--- backend/src/main/java/com/travel/controller/BookingController.java
+++ backend/src/main/java/com/travel/controller/BookingController.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/bookings")
+@RequestMapping("/bookings")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class BookingController {
